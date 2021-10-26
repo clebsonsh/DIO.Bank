162d4a9db838d961133b0793b23f3b639868ce37
Clebson Moura <clebson.sh@gmail.com>
2021-10-26 14:38:09 -0300

    Basic Account Class done

---
+++ DIO.Bank/Classes/Account.cs
@@ -0,0 +1,19 @@
+namespace DIO.Bank
+{
+    public class Account
+    {
+        //Attributes
+        private string Name { get; set; }
+        private double Balance { get; set; }
+
+        // Constructor
+        public Account(string name, double balance)
+        {
+            this.Name = name;
+            this.Balance = balance;
+        }
+
+        // Methods
+        // To do...
+    }
+}
